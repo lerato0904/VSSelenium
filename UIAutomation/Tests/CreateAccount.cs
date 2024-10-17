ce5994cbf39f21677ec3b9488a4bc96fc4b03e72
radebelerato@gmail.com <radebelerato@gmail.com>
2024-10-17 10:40:33 +0200

    remove unnecessary usings

--- UIAutomation/Tests/CreateAccount.cs
+++ UIAutomation/Tests/CreateAccount.cs
@@ -1,14 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.WaitHelpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UIAutomation.Tests
 {
